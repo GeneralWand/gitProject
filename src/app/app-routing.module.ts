e1e99547848690e2ae40feacb7ba18ee84981330
Wandile <generalwandile41@gmail.com>
2023-07-20 14:21:28 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'push',
+    loadChildren: () => import('./push/push.module').then( m => m.PushPageModule)
   }
 ];
 @NgModule({
